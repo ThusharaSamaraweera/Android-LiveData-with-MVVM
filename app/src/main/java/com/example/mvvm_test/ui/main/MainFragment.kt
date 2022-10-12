74e5bc1e21cfa623d16832b25441d5b38c72eb45
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-10-12 23:53:05 +0530

    print entered value

--- app/src/main/java/com/example/mvvm_test/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mvvm_test/ui/main/MainFragment.kt
@@ -2,10 +2,15 @@ package com.example.mvvm_test.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.mvvm_test.R
 
 class MainFragment : Fragment() {
@@ -26,7 +31,30 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        var textInput = view?.findViewById<EditText>(R.id.TextInput);
+        var textOutput = view?.findViewById<TextView>(R.id.TextOutput)
+
+        viewModel.text.observe(viewLifecycleOwner){
+            textOutput?.text = it.toString()
+        }
+
+        textInput?.addTextChangedListener( object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                viewModel.updateText(s.toString())
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.updateText(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                viewModel.updateText(s.toString())
+            }
+
+        })
+
+
     }
 
 }
--- app/src/main/java/com/example/mvvm_test/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/mvvm_test/ui/main/MainViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.mvvm_test.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private var _text = MutableLiveData<String>().apply {
+        value = ""
+    }
+    var text : LiveData<String> = _text
+
+    fun  updateText(updatedText: String){
+        _text.value = updatedText
+    }
 }
